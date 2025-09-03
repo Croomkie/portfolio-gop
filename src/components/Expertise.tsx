e27c39cbfa89f103dcf9d7559315687b11e4b9ca
Lucas <94015198+Croomkie@users.noreply.github.com>
2025-09-03 23:25:23 +0200

    Update Expertise.tsx

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -69,7 +69,7 @@ function Expertise() {
                 <div className="skill">
                     <FontAwesomeIcon icon={faPython} size="3x"/>
                     <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have hands-on experience developing an AI-powered search system using Retrieval-Augmented Generation (RAG) to deliver smarter and more accurate results.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
